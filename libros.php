5e8ef10c87739ef41a9c759d340c908600937bed
ivanmaga1 <ivanmagabrea@gmail.com>
2025-03-16 17:50:49 +0100

    Update libros.php

--- libros.php
+++ libros.php
@@ -16,6 +16,7 @@ $resultado = $conexion->query($query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Libros y Cómics Disponibles</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>Libros y Cómics Disponibles para Alquiler</h1>
