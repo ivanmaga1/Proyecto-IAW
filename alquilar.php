d7a5d33daee01846851f35bc2650278fde9d5e59
ivanmaga1 <ivanmagabrea@gmail.com>
2025-03-16 17:49:37 +0100

    Update alquilar.php

--- alquilar.php
+++ alquilar.php
@@ -30,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alquilar un libro</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>Alquilar un Libro o Cómic</h1>
