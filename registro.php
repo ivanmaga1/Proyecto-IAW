de5f9b5a2410749b67a39862462b23bb7283507a
ivanmaga1 <ivanmagabrea@gmail.com>
2025-03-16 17:51:23 +0100

    Update registro.php

--- registro.php
+++ registro.php
@@ -34,6 +34,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de usuario</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>Formulario de Registro</h1>
